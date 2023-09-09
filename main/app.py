fb2e3b17f47d3597f1d58caef105d3d20196a95f
Precious000 <israelprecious62@gmail.com>
2023-09-09 21:09:02 +0000

    latest

--- main/app.py
+++ main/app.py
@@ -12,14 +12,12 @@ def get_info():
 
     current_day = datetime.datetime.utcnow().strftime('%A')
     current_utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%-d %H:%M:%S')
-<<<<<<< HEAD
 
     github_url_file = "https://github.com/Precious000/HNGx-Stage1-Backend-Task/blob/master/main/app.py"
     github_url_source = "https://github.com/Precious000/HNGx-Stage1-Backend-Task"
-=======
+
     github_url_file = 'https://github.com/Precious000/HNGx-Stage1-Backend-Task/blob/master/main/app.py'
     github_url_source = 'https://github.com/Precious000/HNGx-Stage1-Backend-Task'
->>>>>>> 51a5401 (new commit)
 
     status_code = 200
 
